756a07b5219647fbcdc35f61d8f00c279852377a
vokborok <korobkovsa@gmail.com>
2020-07-20 03:15:10 +0300

    Create pointer.c
    pointer usage with & and * operators

---
+++ pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void go_south_east (int *lat, int *lon)
+{
+  *lat=*lat-1;
+  *lon=*lon +1;
+}
+
+int main()
+{
+  int latitude = 32;
+  int longitude = -64;
+
+  go_south_east(&latitude, &longitude);
+  printf("Stop! Now our coordinates are: [%i, %i]\n", latitude, longitude);
+  return 0;
